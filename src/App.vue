2e055052e3260040a45d8222b99959668f9289c9
Olawuyi Abdulkareem <47379564+villarnovar@users.noreply.github.com>
2020-03-18 23:43:04 +0100

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,49 @@
 <template>
-<div id="app">
+  <div id="app">
+    <Header />
+       
+        <div class="container-fluid mt-3 main">
+          <div class="row">
+            <div class="col-sm-3 cat-div">
+              <ul class="list-group li-div">
+                <li class="list-group-item active">Categories</li>
+              </ul>  
+              <Category/>
+            </div>
+            <div class="col-lg-6 carousel-div">
+              <Imageslider />
+            </div>
+            <div class="col-lg-3 mt-3">
+              <div class="card">
+        
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="container mt-4 product-div">
+          </div>
+                <Product />
+        </div>
   
-  <Header />
-  <div id="container">
-    <div id="firstside">
-      <category />
-    </div>
-    
-    <product />
-  </div>
-  
-</div>
+
 </template>
+  
 
 <script>
 // @ is an alias to /src
 import Header from '@/components/Header'
 import Category from '@/components/Category'
 import Product from '@/components/Product'
-
+import Imageslider from '@/components/Imageslider'
 
 export default {
   name: 'Home',
   components: {
     Header,
     Category,
-    Product
+    Product,
+    Imageslider
     
   }
 
@@ -33,11 +51,6 @@ export default {
 </script>
 
 <style scoped>
-  #container{
-    display: grid;
-    grid-template-columns: 50vh 90vh 1fr;
-    height: 100vh;
 
-}
 
 </style>
---
+++ src/components/Imageslider.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+      <div>
+  <b-carousel
+    id="carousel-fade"
+    style="text-shadow: 0px 0px 2px #000; width"
+    fade
+    indicators
+    img-width="600"
+    img-height="250"
+  >
+    <b-carousel-slide
+      img-src="https://jf-na-cdn.justfab.com/image/site/en_US/brand/ftv/5074/0311_offer1.jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      img-src="https://jf-na-cdn.justfab.com/image/site/en_US/brand/ftv/5074/0311_cta1.jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      img-src="https://images.wallpaperscraft.com/image/boots_legs_shoes_116191_2560x1440.jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      img-src="https://s1.1zoom.me/b5050/141/Coffee_Book_Boots_Jeans_452278_3840x2400.jpg"
+    ></b-carousel-slide>
+  </b-carousel>
+</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Imageslider'
+    
+}
+</script>
+
+<style scoped>
+</style>
